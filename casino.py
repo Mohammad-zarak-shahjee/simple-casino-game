3613dfc1e61f3ae80d0d9f65e8a093addcd89c58
malikjahanzaib <46221334+malikjahanzaib@users.noreply.github.com>
2019-02-11 23:41:09 +0530

    Update casino.py
    A simple card guessing game!

--- casino.py
+++ casino.py
@@ -7,7 +7,7 @@ def play(bet):
 	print('.....*****SHUFFLING*****.....')
 	random.shuffle(cards)
 	playerguess=int(input('Guess the position of QUEEN >>\n\n  1\n\n  2\n\n  3\n\n'))
-	if(cards[playerguess]=='Q'):
+	if(cards[playerguess-1]=='Q'):
 		cash += 3*bet
 		print('WOHOOOOO!!!\nYOU WIN!!!\nSEQUENCE IS {} {} {}\nTOTAL CASH={}'.format(cards[0],cards[1],cards[2],cash))
 	else:
